8a1ae4f550a0452b37074f637adff26eb9692688
zhousulong <parabola@vip.qq.com>
2026-07-30 10:18:33 +0800

    fix: 注册 @unhead/vue createHead，修复 print 页面 usehead injection 报错

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './locale'
+import { createHead } from '@unhead/vue'
 
 const app = createApp(App)
+const head = createHead()
 app.use(router)
 app.use(i18n)
+app.use(head)
 app.mount('#app')
+
